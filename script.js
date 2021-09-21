874ebf024e9f92296c760295d31f903d9d19b304
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-09-21 17:51:04 -0500

    closest to target function

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ let currentRoundNumber = 1;
 
 // Function to generate a random number between 0 and 9
 const generateTarget = () => {
-    return Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * 10);
 };
 
+// Function to determine which guess is closest to the target number
+
+const compareGuesses = (user, computer, target) => {
+  if (Math.abs(target - user) > Math.abs(target - computer)) {
+    return false;
+  } else if (Math.abs(target - user) <= Math.abs(target - computer)) {
+    return true;
+  }
+};
