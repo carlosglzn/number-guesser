d6561deb1e514384028948f4c682a7b427870741
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-09-21 17:31:02 -0500

    random number function

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+// Function to generate a random number between 0 and 9
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+};
+
